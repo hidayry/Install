8b34213a37b168b2fdae6844ef119faab341cbdf
hidayry <ryan.hidayat77@gmail.com>
2023-09-14 09:25:29 +0700

    update change

--- i3lock_debian.sh
+++ i3lock_debian.sh
@@ -2,3 +2,5 @@
 
 #Run apt
 sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
+
+
